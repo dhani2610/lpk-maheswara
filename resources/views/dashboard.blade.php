45db51073c49bf75cad4bf638173fbaa3aec39fe
andakagdv <andaka@goodeva.co.id>
2026-02-24 13:36:08 +0700

    fix : update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
                 <div class="card p-5 text-center bg-white border-0 shadow-sm">
                     <div class="card-body">
                         <i class="fa-solid fa-hands-clapping fs-1 text-danger mb-3"></i>
-                        <h2 class="fw-bold text-dark">Selamat Datang, Andaka!</h2>
+                        <h2 class="fw-bold text-dark">Selamat Datang</h2>
                         <p class="text-muted">Ini adalah pusat kendali untuk mengelola website LPK Maheswara.</p>
                     </div>
                 </div>
